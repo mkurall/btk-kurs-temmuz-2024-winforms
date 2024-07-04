d8549b2ee4ebf849efecfeb4359380a14b944298
Mustafa KURAL <mustafakural@outlook.com>
2024-07-04 14:06:36 +0300

    duzenleme

--- d04_tic_tac_toe/Form1.cs
+++ d04_tic_tac_toe/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms.VisualStyles;
+
 namespace d04_tic_tac_toe
 {
     public partial class Form1 : Form
@@ -34,19 +36,52 @@ namespace d04_tic_tac_toe
                 lblOyuncu1.ForeColor = Color.Black;
             }
         }
+       
+        bool KontrolEt()
+        {
+            Image img = siraKimde == 0 ?  pictureBox1.Image : pictureBox2.Image;
+
+            if (btn00.Image == img && btn01.Image == img && btn02.Image == img ||
+                btn00.Image == img && btn11.Image == img && btn22.Image == img ||
+                btn00.Image == img && btn10.Image == img && btn20.Image == img ||
+                btn01.Image == img && btn11.Image == img && btn21.Image == img ||
+                btn02.Image == img && btn12.Image == img && btn22.Image == img ||
+                btn10.Image == img && btn11.Image == img && btn12.Image == img ||
+                btn20.Image == img && btn21.Image == img && btn22.Image == img ||
+                btn02.Image == img && btn11.Image == img && btn20.Image == img)
+            return true;
+
+            return false;
+        }
 
         private void btn00_Click(object sender, EventArgs e)
         {
-            //
+            //bütün butonlar týklandýðýndA burasý çalýþýyorsa
+            //hangisi týklandý? sender
+            Button btnTiklanan = (Button)sender; //int = (int)double
 
-            if(siraKimde==0)
+            if (siraKimde==0)
             {
+                btnTiklanan.BackgroundImage = pictureBox1.Image;
+                btnTiklanan.BackgroundImageLayout = ImageLayout.Stretch;
+                btnTiklanan.Enabled = false;//pasif
+                //kazandým mý? kontrol et
+
+
                 siraKimde = 1;
             }
             else
             {
+                btnTiklanan.BackgroundImage = pictureBox2.Image;
+                btnTiklanan.BackgroundImageLayout = ImageLayout.Stretch;
+                btnTiklanan.Enabled = false;
+
+
+
                 siraKimde = 0;
             }
+
+
 
             SiraGoster();
         }
